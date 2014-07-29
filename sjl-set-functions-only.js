48faa04c4153b04a20b0444822038f2219520fd4
Ely De La Cruz <elycruz@elycruz.com>
2014-07-28 23:14:07 -0400

    Listed Input, InputFilters, and Validators in readme.

--- sjl-set-functions-only.js
+++ sjl-set-functions-only.js
@@ -1,4 +1,4 @@
-/**! sjl-set-functions-only.js Mon Jul 28 2014 22:48:26 GMT-0400 (Eastern Daylight Time) **//**
+/**! sjl-set-functions-only.js Mon Jul 28 2014 22:55:31 GMT-0400 (Eastern Daylight Time) **//**
  * Created by Ely on 5/24/2014.
  * ** Cartesian functions copied from "Javascript the definitive guide"
  * ** getValueFromObj and setValueOnObj are not from "Javascript ..."
