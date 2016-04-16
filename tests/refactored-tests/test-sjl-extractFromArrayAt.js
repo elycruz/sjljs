b7c81179ca158a2e178eb726609ebffff427be78
Ely De La Cruz <elycruz@elycruz.com>
2016-04-16 00:38:21 -0400

    Progress on extractFromArrayAt tests.

--- tests/refactored-tests/test-sjl-extractFromArrayAt.js
+++ tests/refactored-tests/test-sjl-extractFromArrayAt.js
@@ -10,26 +10,30 @@ var chai = require('chai'),
     expect = chai.expect;
 // ~~~ /STRIP ~~~
 
-describe ('#sjl.extractFromArrayAt', function () {
+describe('#sjl.extractFromArrayAt', function () {
 
     'use strict';
 
-    it ('should return an array containing the extracted value and the resulting spliced array.', function () {
+    it('should return an array containing the extracted value and the resulting spliced array.', function () {
 
     });
 
-    it ('should return a [new array by default and return the extracted value .', function () {
-        var testArray = ['a', 'b', 'c', 'd', 'e'];
-        testArray.forEach(function (elm, index, list) {
-            var result = sjl.extractFromArrayAt(list, index),
-                extractedValue = result[0],
-                splicedArray = result[1];
+    var testArray = ['a', 'b', 'c', 'd', 'e'],
+        stringOfTestArray = '[' + testArray.join(',') + ']';
+    testArray.forEach(function (elm, index, list) {
+        var result = sjl.extractFromArrayAt(list, index),
+            extractedValue = result[0],
+            splicedArray = result[1];
+        it('should extract "' + list[index] + '" from `' + stringOfTestArray + '`.', function () {
             expect(extractedValue).to.equal(list[index]);
-            expect(splicedArray).to.not.equal(list);
-            expect(splicedArray.length).to.not.equal(list.length);
-            expect(splicedArray.indexOf(extractedValue)).to.equal(-1);
-            console.log(result);
         });
+        it('should return a new array by default and return the extracted value .', function () {
+        });
+
+        expect(splicedArray).to.not.equal(list);
+        expect(splicedArray.length).to.not.equal(list.length);
+        expect(splicedArray.indexOf(extractedValue)).to.equal(-1);
+        console.log(result);
     });
 
 });
