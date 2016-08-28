969c57c89a81c932fb547134f3d44b98c1b32ed3
Ely De La Cruz <elycruz@elycruz.com>
2016-08-28 09:54:02 -0400

    Updated documentation for Extendable.

--- src/stdlib/Extendable.js
+++ src/stdlib/Extendable.js
@@ -20,14 +20,14 @@
      * Extends the passed in constructor with `Extendable`.
      * @examples
      * // Scenario 1: function called with a constructor, a prototype object, and a static properties object.
-     * // 2nd and 3rd args are optional in this scenario
+     * // 2nd and 3rd arguments are optional in this scenario
      * Extendable.extend(SomeConstructor, somePrototypeHash, someStaticPropsHash);
      *
      * // Scenario 2: function is called with a prototype object and a static properties object
-     * // 2nd arg is optional in this scenario.
+     * // 2nd argument is optional in this scenario.
      * // Note: First arg must contain a constructor property containing the constructor to extend which is also
-     * //   the constructor that gets set on actual prototype of the extended method which then becomes
-     * //   un-writable/un-configurable (makes for solid oop).
+     * //   the constructor that gets set on said constructors prototype.constructor property
+     * //   (gets set as un-writable/un-configurable makes for a more accurate oop experience).
      * Extendable.extend({ constructor: SomeConstructor, someMethod: function () {} },
      *                   {someStaticProp: 'hello'});
      * @see sjl.defineSubClass
